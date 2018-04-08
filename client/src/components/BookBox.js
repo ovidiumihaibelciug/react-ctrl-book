537d46df2300c7d525f40ce12a73bde08978de5f
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2018-04-08 23:12:48 +0300

    Text bug fixed

--- client/src/components/BookBox.js
+++ client/src/components/BookBox.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react'
 import Rating from '../components/Rating'
 
 export default class BookBox extends Component {
+    parseText = (text, limit) => {
+        if (text === '') {
+            return '';
+        }
+        if (text.length > limit)
+            for (let i = limit; i > 0; i--){
+                if(text.charAt(i) === ' ' && (text.charAt(i-1) != ','||text.charAt(i-1) != '.'||text.charAt(i-1) != ';')) {
+                    return text.substring(0, i) + '...';
+                }
+            }
+        else
+        return text;
+    };
     render() {
         let {volumeInfo} = this.props.book;
         let rating = !volumeInfo.averageRating ? 0 : Math.floor(volumeInfo.averageRating);
+        let authors = [];
+        volumeInfo.authors.forEach(author => {
+            authors = [...authors, author];
+        })
+        authors = authors.join(', ');
         return (
             <div className="book-box">                
                 <div className="left-side">
@@ -13,13 +31,13 @@ export default class BookBox extends Component {
                 </div>
                 <div className="right-side">
                     <div className="settings"><i className="fa fa-ellipsis-v fa-lg"></i></div>
-                    <div className="title">{volumeInfo.title}</div>
-                    <div className="author">by {volumeInfo.authors.map(author => author)}</div>
+                    <div className="title">{this.parseText(volumeInfo.title, 50)}</div>
+                    <div className="author">by {this.parseText(authors, 50)}</div>
                     <div className="rating">
                         <Rating score={rating} /> {!rating ? ' No votes' : ''}
                     </div>
                     <div className="description">
-                        {volumeInfo.description}
+                        {this.parseText(volumeInfo.description ? volumeInfo.description : '', 120)}
                     </div>
                 </div>
                 <div className="likes">
